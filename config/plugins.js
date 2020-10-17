c9be9aa95e0261715694fb80f4cc615150e18b1b
David Erickson <daviderickson@davids-mbp.lan>
2020-10-17 06:33:55 -0400

    intial commit

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+// module.exports = ({ env }) => ({
+//     // ...
+//     upload: {
+//       provider: 'cloudinary',
+//       providerOptions: {
+//         cloud_name: env('erickson-galeazzi'),
+//         api_key: env('148426624887321'),
+//         api_secret: env('DdlGCYM6GA2VSTnkRdVlP4_DWLE'),
+//       },
+//     },
+//     // ...
+//   });
